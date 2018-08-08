bf3461b3fc9924dbd874df059b0a88ccbadd5b0a
marshallstan <413428648@qq.com>
2018-08-08 22:21:07 +0800

    hot-module-replacement

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -11,7 +11,7 @@ const config = {
   output: {
     filename: '[name].[hash].js',
     path: path.join(__dirname, '../dist'),
-    publicPath: '/public'
+    publicPath: '/public/'
   },
   module: {
     rules: [
@@ -47,7 +47,7 @@ if (isDev) {
     contentBase: path.join(__dirname, '../dist'),
     hot: true,
     overlay: {errors: true},
-    publicPath: '/public',
+    publicPath: '/public/',
     historyApiFallback: {index: '/public/index.html'}
   };
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
--- client/app.js
+++ client/app.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {AppContainer} from 'react-hot-loader';
+import {AppContainer} from 'react-hot-loader'
 import App from './App.jsx'
+
+let renderMethod = module.hot ? ReactDom.render : ReactDom.hydrate;
+const root = document.getElementById('root');
+const render = Component => {
+  renderMethod(
+    <AppContainer>
+      <Component />
+    </AppContainer>, root
+  )
+};
+
+render(App);
 
 if (module.hot) {
   module.hot.accept('./App.jsx', () => {
     const NextApp = require('./App.jsx').default;
-    ReactDom.render(<NextApp />, document.getElementById('root'));
+    render(NextApp);
   })
-} else {
-  ReactDom.hydrate(<App />, document.getElementById('root'));
 }
