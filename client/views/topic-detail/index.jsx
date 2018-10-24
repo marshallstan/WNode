d9808802b3e804407199e4d0fa19d0f2ace1cacd
marshall wu <marshall.wu@cssinco.cn>
2018-10-24 17:24:52 +0800

    some code of page detail

--- client/views/topic-detail/index.jsx
+++ client/views/topic-detail/index.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+// import PropTypes from 'prop-types'
+// import marked from 'marked'
+// import Helmet from 'react-helmet'
+import {
+  inject,
+  observer
+} from 'mobx-react'
 
-export default class TopicDetail extends React.Component {
+// import { withStyles } from '@material-ui/core/styles'
+// import Paper from '@material-ui/core/Paper'
+// import { CircularProgress } from '@material-ui/core'
+
+import Container from '../layout/container'
+
+// import { topicDetailStyle } from './styles'
+// import Reply from './reply'
+
+@inject(stores => ({
+  topicStore: stores.topicStore
+}))
+@observer
+class TopicDetail extends React.Component {
   componentDidMount() {}
   render() {
+    // const { classes } = this.props
+    // const topic = this.props.topicStore.topics[0]
+    // if (!topic) {
+    //   return (
+    //     <Container>
+    //       <section className={classes.loadingContainer}>
+    //         <CircularProgress color="accent" />
+    //       </section>
+    //     </Container>
+    //   )
+    // }
     return (
-      <div>This is topic detail.</div>
+      <div>
+        <Container>
+          6
+        </Container>
+      </div>
     )
   }
 }
+
+export default TopicDetail
